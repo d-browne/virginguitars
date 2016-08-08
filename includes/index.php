bcff5ba93e0075f7f5298e4b044361ea82597c1f
d-browne <d.browne@hackplex.com>
2016-08-08 16:18:23 +1000

    Created includes directory index page
    Created a standard php redirect page as the index of the includes
    directory for added security

---
+++ includes/index.php
@@ -0,0 +1,6 @@
+<?php
+// This is a blank redict page to redict to the root of the website
+// This page is add extra security to the includes directory
+
+header('Location: http://localhost/virginguitars'); // Redirect to homepage
+?>
