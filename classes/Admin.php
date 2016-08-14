f74ac7290689700420578208b1678b57648c73f9
d-browne <d.browne@hackplex.com>
2016-08-15 01:01:21 +1000

    Created Admin Class Stub
    Not yet completed

--- classes/Admin.php
+++ classes/Admin.php
@@ -1,8 +1,30 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+class Admin 
+{
+    private $email;
+    private $password;
+    private $isAuthenticated;
+    
+    // Construct
+    function __construct($email, $password)
+    {
+        return authenticate($email, $password);
+    }
+    
+    // Function to authenticate admin
+    public function authenticate($email, $password)
+    {
+        $this->email = $email;
+        $this->password-> $password;
+        // TODO 
+        // Check email and password against database
+        // If email and password match database set authenticated to true
+    }
+    
+    // Function to get authenticated status
+    public function getAuthenticated()
+    {
+        return $isAuthenticated;
+    }
+}
