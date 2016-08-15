af688f918e07a33fcf65c6fa6b51adab41416169
d-browne <d.browne@hackplex.com>
2016-08-15 20:23:22 +1000

    Added customer login function and unit test

---
+++ classes/Customer.php
@@ -0,0 +1,53 @@
+<?php
+
+class customer
+{
+    
+    // Class Constructor
+    function __construct()
+    {
+        
+    }
+    
+    public function login($email, $password)
+    {
+        // Get a data connection
+        $database = new Database();
+        $dataConnection = $database->getDataConnection();
+        
+        // query for email password and salt
+        $query = "SELECT Email,EncryptedPassword,Salt FROM customer";
+        // Execute query
+        $results = $dataConnection->query($query);
+        
+        // If there are no results for the query (table empty) return false (customer is not in database)
+        if ($results == NULL)
+        {
+            return false;
+        }
+        
+        // If there is more than 0 results
+        if ($results->num_rows > 0)
+        {
+            // Loop through each row
+            while ($row = $results->fetch_assoc())
+            {
+                // Hash the password against table salt
+                $hashedPassword = hash("sha256", $password . $row["Salt"]);
+                
+                // Check uemail and hashed password against database (row) values
+                if ($email == $row["Email"] && $hashedPassword == $row["EncryptedPassword"])
+                {
+                    return true; // User is found in databse
+                }
+            }
+            return false;
+        }
+        else // Zero results
+        {
+            return false; // Customer not in table
+        }
+
+        
+    }
+}
---
+++ tests/CustomerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require 'classes/Customer.php';
+
+class CustomerTest extends TestCase
+{
+    // Test data for login/varify password test
+    // Warning!!!: these tests are designed to pass with testing database
+    // May not pass after database alterations
+    public function customerLoginDataProvider()
+    {
+        return array(
+          array("ben@mail.com", "password1", TRUE),                         // Valid login
+          array("dale@mail.com", "password2", TRUE),                        // Valid login
+          array("warren@mail.com", "password3", TRUE),                      // Valid login
+          array("dominic@mail.com", "password4", TRUE),                     // Valid login
+          array("davidmullburry@fitzroy.org", "delicious", FALSE),          // Invalid login, customer doesn't exist
+          array("checkzFuzzlewuz@bundurry.org.au", "CranyVuzxcf", FALSE),   // Invalid login, cusomter doesn't exist
+          array("", "", FALSE),                                             // Invalid login, empty strings
+          array(1, 2, FALSE),                                               // Invalid login, integers
+          array(NULL, NULL, FALSE),                                         // Invalid login, null values
+          array("'DROP TABLE customers", NULL, FALSE)                       // Invalid login, null value and simulated attack string
+        );
+    }
+    
+    /**
+     * 
+     * @dataProvider customerLoginDataProvider
+     */
+    public function testLogin($username, $password, $expected)
+    {
+        // Customer object to test logins
+        $customer = new Customer();
+        
+        // Test logins
+        $this->assertEquals($expected, $customer->login($username, $password));
+    }
+}
