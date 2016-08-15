74abbca245dac65b374fa0cb955e119ebcac9db2
d-browne <d.browne@hackplex.com>
2016-08-15 23:55:40 +1000

    Implemented New cusomter function
    Wrote new customer function and unit test

--- classes/Customer.php
+++ classes/Customer.php
@@ -1,8 +1,116 @@
 <?php
 
-class customer
+class Customer
 {
+    private $LastName;
+    private $FirstName;
+    private $Salutation;
+    private $MailingList;
+    private $Email;
+    private $MobilePhone;
+    private $HomePhone;
+    private $hashedPassword;
+    private $salt;
     
+    // Function to delete customer from databse
+    public static function deleteCusomter($email) 
+    {
+        // Check if customer exists in database
+        if(Customer::doesCustomerExist($email))
+        {
+            
+        }
+    }
+    
+    // Returns true if customer created and false if not
+    public static function newCustomer($email, $password)
+    {
+        // Check if customer already exists
+        if (Customer::doesCustomerExist($email))
+        {
+            return false;
+        }
+        
+        // Check if email invalid
+        if(filter_var($email, FILTER_VALIDATE_EMAIL) == false)
+        {
+            return "invalid email";
+        }
+        
+        // Populate default vauls
+        $Email = $email; // Set email
+        $LastName = "";
+        $FirstName = "";
+        $Salutation = "";
+        $MailingList = 0;
+        
+        // Generate hashed password and salt
+        $salt = mt_rand(-4000,2147483647); // Generate random salt for database
+        $hashedPassword = hash ("sha256", $password . $salt);
+        
+        // create a data connection
+        $database = new Database();
+        $dataConnection = $database->getDataConnection();
+        
+        // Query to add user to database
+        $query = "INSERT INTO customer VALUES (DEFAULT, '".
+                mysqli_real_escape_string($dataConnection, $LastName) ."', '".
+                mysqli_real_escape_string($dataConnection, $FirstName) ."', '".
+                mysqli_real_escape_string($dataConnection, $Salutation) ."', ".
+                mysqli_real_escape_string($dataConnection, $MailingList) .", '".
+                mysqli_real_escape_string($dataConnection, $Email) ."', '".
+                mysqli_real_escape_string($dataConnection, $hashedPassword) ."', ".
+                mysqli_real_escape_string($dataConnection, $salt) .", NULL, NULL)";
+
+        // Execute query
+        $dataConnection->query($query);
+        
+        // Check if customer was created successfully
+        if (Customer::validateCustomer($email, $password))
+        {
+            return true;
+        }
+        else
+        {
+            die(); // This should never happen.
+        }
+    }
+    
+    public function getHomePhone()
+    {
+        return $this->HomePhone;
+    }
+    
+    public function getMobilePhone()
+    {
+        return $this->MobilePhone;
+    }
+    
+    public function getEmail()
+    {
+        return $this->Email;
+    }
+    
+    public function isMailingList()
+    {
+        return $this->MailingList;
+    }
+    
+    public function getSalutation()
+    {
+        return $this->Salutation;
+    }
+    
+    public function getFirstName()
+    {
+       return $this->FirstName;
+    }
+    
+    
+    public function getLastName()
+    {
+        return $this->LastName;
+    }
     
     // Class Constructor
     function __construct()
@@ -10,8 +118,29 @@ class customer
         
     }
     
+    // Function to check if customer exists
+    public static function doesCustomerExist($email)
+    {
+        // create a data connection
+        $database = new Database();
+        $dataConnection = $database->getDataConnection();
+        
+        
+        
+        // Query to select email address
+        $query = "SELECT Email FROM customer WHERE Email = '".mysqli_real_escape_string($dataConnection, $email)."'";
+        $result = $dataConnection->query($query);
+        
+        // Check if result is set
+        if ($result->num_rows > 0)
+        {
+            return true;
+        }
+        return false; 
+    }
+
     // Function to check if user exists in database
-    public function checkDatabaseForUser($email, $password)
+    public static function validateCustomer($email, $password)
     {
         // Get a data connection
         $database = new Database();
--- tests/CustomerTest.php
+++ tests/CustomerTest.php
@@ -29,12 +29,59 @@ class CustomerTest extends TestCase
      * 
      * @dataProvider customerLoginDataProvider
      */
-    public function testCheckDatabaseForUser($username, $password, $expected)
+    public function testValidateCustomer($username, $password, $expected)
     {
-        // Customer object to test logins
-        $customer = new Customer();
-        
         // Test logins
-        $this->assertEquals($expected, $customer->checkDatabaseForUser($username, $password));
+        $this->assertEquals($expected, Customer::validateCustomer($username, $password));
+    }
+    
+    public function doesExistDataProvider()
+    {
+        return array(
+            array("dominic@mail.com", true),        // Exists in database
+            array("ben@mail.com", true),            // Exists in database
+            array("dale@mail.com", true),           // Exists in database
+            array("warren@mail.com", true),         // Exists in database
+            array("do2minic@mai2l.com", false),     // Does not exist in database
+            array("", false),                       // blank string
+            array(88, false),                       // Integer
+            array("dsadfsdfdsf", false),            // random junk
+            array("' DROP TABLE customer", false),  // Database breaking stuff
+            array("\'dsfsdf'dfsdf  'd  dfsdf '\dsf sd\\", false)
+        );
+    }
+    
+    /**
+     * @dataProvider doesExistDataProvider
+     */
+    public function testDoesCustomerExist($email, $expected)
+    {
+        $this->assertEquals($expected, Customer::doesCustomerExist($email));
+    }
+    
+    public function newCustomerDataProvider()
+    {
+        return array(
+            array("dominic@mail.com", "password1", false),
+            array("dominic@mail.com", "password2", false),
+            array("ben@mail.com", "passwo434rd2", false),
+            array("dale@mail.com", "pa324234ord2", false),
+            array("warren@mail.com", "passwo234324rd2", false),
+            array("warren@mail.com", "pas'; swo234324rd2", false),
+            array("flipper", "I'm swole", "invalid email"),
+            array("flipper@shingoxc", "I'm note swole!!", "invalid email"),
+            array("newguy@mail.com", "Passiwassi11", true),
+            array("newguy2@mail.org", "Passiwassi11", true),
+            array("newguy3@mail.net", "Passiwassi11", true),
+            array("newguy4@mail.com.au", "Passiwassi11", true)  
+        );
+    }
+    
+    /**
+     * @dataProvider newCustomerDataProvider
+     */
+    public function testNewCustomer($email, $password, $expected)
+    {
+        $this->assertEquals($expected, Customer::newCustomer($email, $password));
     }
 }
