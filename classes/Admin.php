ca6c51404eb1a7fb83f75ef5d5510d92002200b9
d-browne <d.browne@hackplex.com>
2016-08-31 16:51:18 +1000

    Changed case in query

--- classes/Admin.php
+++ classes/Admin.php
@@ -22,7 +22,7 @@ class Admin
     // Function to authenticate admin
     public function authenticate($username, $password)
     {
-        $query = "SELECT UserName,EncryptedPassword,Salt FROM administrator";
+        $query = "SELECT UserName,EncryptedPassword,Salt FROM ADMINISTRATOR";
         $result = $this->connection->query($query);
            
         
