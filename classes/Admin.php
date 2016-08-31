dff0229f63735c8bd22dac6cac1727255dd3852a
d-browne <d.browne@hackplex.com>
2016-08-31 15:36:00 +1000

    Updated Requires
    Fixes tests

--- classes/Admin.php
+++ classes/Admin.php
@@ -1,6 +1,6 @@
 <?php
 
-require 'classes/Database.php';
+require_once 'classes/Database.php';
 
 class Admin 
 {
--- classes/Customer.php
+++ classes/Customer.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'classes/Database.php';
+
 class Customer
 {
     private $LastName;
