e19d1b80addeea2f52e1fb840502d8be6ceeaeb7
d-browne <d.browne@hackplex.com>
2016-09-05 03:02:58 +1000

    FAQ class completed

--- classes/FAQ.php
+++ classes/FAQ.php
@@ -8,27 +8,59 @@ class FAQ
     function __construct()
     {
         // Check if FAQ data file exists (specified in Global Settings)
-        if (file_exists(PATH_TO_FAQ))
+        if (file_exists(GlobalSettings::PATH_TO_FAQ))
         {
             // Do nothing. 
         }
         else
         {
             // Create page
-            // if not initialize with blank data "<b>faq goes here</b>"
-            $this->setData("<b>faq goes here</b>");
+            try 
+            {
+                $faqDataFile = fopen(GlobalSettings::PATH_TO_FAQ, "w");
+                // if not initialize with blank data "<b>faq goes here</b>"
+                fwrite($faqDataFile, "<b>faq goes here</b>");
+                fclose($faqDataFile);
+                return true; // Everything OK
+                
+            } catch (Exception $ex) {
+                return $ex;
+            }         
         }
     }
     
     // function to get faq file data
     public function getData()
     {
-        // Return the file contents
+        try {
+            $faqDataFile = fopen(GlobalSettings::PATH_TO_FAQ, "r");
+            return fread($faqDataFile, filesize(GlobalSettings::PATH_TO_FAQ));
+        } catch (Exception $ex) {
+            return $ex;
+        }
+        // Return file contents
     }
     
     // Function to set faq file data
     function setData($data)
     {
         // Update page
+        try {
+            $faqDataFile = fopen(GlobalSettings::PATH_TO_FAQ, "w");
+            return fwrite($faqDataFile, $data);
+        } catch (Exception $ex) {
+            return $ex;
+        }
+    }
+    
+    // Function to delete faq file from disk
+    function deleteFile()
+    {
+        try
+        {
+            return unlink(GlobalSettings::PATH_TO_FAQ);
+        } catch (Exception $ex) {
+            return $ex;
+        }
     }
 }
