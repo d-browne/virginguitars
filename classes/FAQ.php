d045727dd574c9d49a50f0e6aaf8fbbb5812f535
d-browne <d.browne@hackplex.com>
2016-09-05 02:00:13 +1000

    Updated class structure stub

--- classes/FAQ.php
+++ classes/FAQ.php
@@ -4,27 +4,31 @@ require_once 'classes/GlobalSettings.php';
 
 class FAQ
 {
-    private $faqData; // Holds faq page data
-    
     // Class constructor
     function __construct()
     {
         // Check if FAQ data file exists (specified in Global Settings)
-        // if not initialize with blank data "<b>faq goes here</b>"
-        
-        // get datafile data to $faqData
+        if (file_exists(PATH_TO_FAQ))
+        {
+            // Do nothing. 
+        }
+        else
+        {
+            // Create page
+            // if not initialize with blank data "<b>faq goes here</b>"
+            $this->setData("<b>faq goes here</b>");
+        }
     }
     
     // function to get faq file data
     public function getData()
     {
-        return $faqData;
+        // Return the file contents
     }
     
     // Function to set faq file data
     function setData($data)
     {
         // Update page
-        // if successful update object variable
     }
 }
