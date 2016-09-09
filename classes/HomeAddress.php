af2f7e868a636ed96b44be9e0bfb6a0109156e0a
d-browne <d.browne@hackplex.com>
2016-09-10 02:57:20 +1000

    Added getters

--- classes/HomeAddress.php
+++ classes/HomeAddress.php
@@ -1,8 +1,47 @@
 <?php
 
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This class represents the HomeAddress object
  */
 
+class HomeAddress
+{
+    private $HomeAddressID;
+    private $CustomerID;
+    private $StreetAddress;
+    private $City;
+    private $State;
+    private $PostCode;
+    private $Country;
+    
+    // Start getters
+    public function getHomeAddressID()
+    {
+        return $this->HomeAddressID;
+    }
+    public function getCustomerID()
+    {
+        return $this->CustomerID;
+    }
+    public function getStreetAddress()
+    {
+        $this->StreetAddress;
+    }
+    public function getCity()
+    {
+        return $this->City;
+    }
+    public function getState()
+    {
+        return $this->State;
+    }
+    public function getPostCode()
+    {
+        return $this->PostCode;
+    }
+    public function getCountry()
+    {
+        return $this->Country;
+    }
+    // End getters
+}
