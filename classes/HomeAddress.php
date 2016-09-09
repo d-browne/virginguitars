88d19496338ba3cbdde07983ee684b1f36af1c98
d-browne <d.browne@hackplex.com>
2016-09-10 04:56:40 +1000

    Created class constructor

--- classes/HomeAddress.php
+++ classes/HomeAddress.php
@@ -23,6 +23,37 @@ class HomeAddress
         // Check if check if customer ID present in HOMEADDRESS table
         // If present create object using values
         // Otherwise create new record with blank values. 
+        
+        // Create data connection
+        $database = new Database();
+        $dataConnection = $database->getDataConnection();
+        
+        // Query to find customer
+        $query = "SELECT * FROM HOMEADDRESS WHERE CustomerFK='".mysqli_real_escape_string($dataConnection, $CustomerID)."';";
+        
+        // Execute query
+        $result = $dataConnection->query($query);
+        
+        // Break execution if query fails
+        if ($result === false)
+        {
+            return;
+        }
+        
+        if ($result->num_rows > 0) 
+        {
+            // Get the first row of result
+            $row = $result->fetch_assoc();
+            
+            // Set local variables
+            $this->HomeAddressID = $row['HomeAddressID'];
+            $this->CustomerID = $row['CustomerFK'];
+            $this->StreetAddress = $row['StreetAddress'];
+            $this->City = $row['City'];
+            $this->State = $row['State'];
+            $this->PostCode = $row['PostCode'];
+            $this->Country = $row['Country'];
+        }
     }
     
     // Start getters
