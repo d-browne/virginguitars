f0d680f13881ce6e3afe4b8c77b578410cb0e0da
d-browne <d.browne@hackplex.com>
2016-09-10 06:11:31 +1000

    Updated class and test

--- classes/HomeAddress.php
+++ classes/HomeAddress.php
@@ -25,6 +25,12 @@ class HomeAddress
         // If present create object using values
         // Otherwise create new record with blank values. 
         
+        /*
+         * Exception codes:
+         * 0 - CustomerID not a number
+         * 1- CustomerID does not match an existing customer
+         */
+        
         // Create data connection
         $database = new Database();
         $dataConnection = $database->getDataConnection();
@@ -35,7 +41,7 @@ class HomeAddress
         // throw if not integer
         if (!is_numeric($CustomerID))
         {
-            throw new Exception("CusomterID is not numeric");
+            throw new Exception("CustomerID is not numeric", 0);
         }
         
         // Query check if customer exists
@@ -52,7 +58,7 @@ class HomeAddress
         
         if ($result->num_rows < 1)
         {
-            throw new Exception("No CustomerID found in DB");
+            throw new Exception("No CustomerID found in DB", 1);
         }
         
         // Query to find customer
@@ -64,7 +70,7 @@ class HomeAddress
         // Break execution if query fails
         if ($result === false)
         {
-            throw new Exception("Query failed");
+            throw new Exception("Getting customer query failed");
         }
         
         if ($result->num_rows > 0) 
@@ -80,6 +86,51 @@ class HomeAddress
             $this->State = $row['State'];
             $this->PostCode = $row['PostCode'];
             $this->Country = $row['Country'];
+        }
+        else
+        {
+            // Create new record for customer
+            $query = "INSERT INTO HOMEADDRESS (CustomerFK) VALUES (".$CustomerID.");";
+            
+            // Execute query 
+            $result = $dataConnection->query($query);
+            
+            // throw if query fails
+            if ($result === false)
+            {
+                throw new Exception("Creating new HOMEADDRESS record failed");
+            }
+            
+            // Query for the newly inserted home address
+            $query = "SELECT * FROM HOMEADDRESS WHERE CustomerFK=".$CustomerID.";";
+
+            // Execute query
+            $result = $dataConnection->query($query);
+
+            // Break execution if query fails
+            if ($result === false)
+            {
+                throw new Exception("Getting customer query failed");
+            }
+
+            if ($result->num_rows > 0) 
+            {
+                // Get the first row of result
+                $row = $result->fetch_assoc();
+
+                // Set local variables
+                $this->HomeAddressID = $row['HomeAddressID'];
+                $this->CustomerID = $row['CustomerFK'];
+                $this->StreetAddress = $row['StreetAddress'];
+                $this->City = $row['City'];
+                $this->State = $row['State'];
+                $this->PostCode = $row['PostCode'];
+                $this->Country = $row['Country'];
+            }
+            else
+            {
+                throw new Exception("Unable to find HOMEADDRESS record"); // This should never be thrown
+            }
         }
     }
     
---
+++ tests/HomeAddressTest.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * Virgin Guitars E-Commerce Website 2016 - HomeAddressTest.php
+ * VGECW TEAM: Dominic Browne, Warren Norris, Dale Hogan, Ben Morrison
+ * 
+ * 
+ * 
+ * This file contains the unit tests to check the HomeAddress class
+ * To run these units tests type phpunit tests/HomeAddressTest.php from the website root
+ * For more details on running phpunit tests see the virgin guitars test plan
+ */
+
+use PHPUnit\Framework\TestCase;
+
+require_once 'classes/HomeAddress.php';
+
+class HomeAddressTest extends TestCase
+{
+    
+    public function testConstruct($customerID, $expectedStreet, $expectedCity, $expectedState, $expectedPostCode, $expectedCountry)
+    {
+        $homeAddress = new HomeAddress($customerID);
+        
+        $this->assertEquals($expectedStreet, $homeAddress->getStreetAddress());
+        $this->assertEquals($expectedCity, $homeAddress->getCity());
+        $this->assertEquals($expectedState, $homeAddress->getState());
+        $this->assertEquals($expectedPostCode, $homeAddress->getPostCode());
+        $this->assertEquals($expectedCountry, $homeAddress->getCountry());
+    }
+    
+    /*
+    * Exception codes:
+    * 0 - CustomerID not a number
+    * 1- CustomerID does not match an existing customer
+    */
+    public function constructExceptionDataProvider()
+    {
+        return array(
+            array(NULL, 0),
+            array("one", 0),
+            array("random gibberish", 0),
+            array("'; DROP TALBE HOMEADDRESS", 0),
+            array(5, 1),
+            array(7, 1),
+            array(8, 1),
+            array(-8, 1)
+        );
+    }
+    
+    /**
+     * 
+     * @dataProvider constructExceptionDataProvider
+     */
+    public function testConstructExceptions($CustomerID, $expectedException)
+    {
+        try
+        {
+            $homeAddress = new HomeAddress($CustomerID);
+        } catch (Exception $ex) {
+            $this->assertEquals($expectedException, $ex->getCode());
+        }
+    }
+}
