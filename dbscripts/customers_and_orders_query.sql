ee90f7d51ff98d954f7673ac1c091696d496fff2
d-browne <d.browne@hackplex.com>
2016-09-11 00:46:01 +1000

    Added customers and orders query

---
+++ dbscripts/customers_and_orders_query.sql
@@ -0,0 +1,10 @@
+# Virgin Guitars E-commerce Website
+# This Query Reutrns a list of all Customers, their details and number of open and closed orders.
+# For use in displaying a list of customers in the Administration panel
+# Query Written by Dominic Browne 11th September 2016
+
+SELECT CustomerID, FirstName, LastName, Email, COUNT(CustomerID) as 'All Orders', SUM(case when order_status.Description = 'Requested' then 1 else 0 end) as 'New Orders'
+FROM CUSTOMER
+JOIN sales_order ON sales_order.CustomerFK = CustomerID
+LEFT JOIN order_status ON order_status.OrderStatusID = sales_order.OrderStatusFK
+GROUP BY CustomerID
