73eff8f190efea7b62ed3ea037055b732d2b66c7
d-browne <d.browne@hackplex.com>
2016-09-13 13:47:30 +1000

    added order unit test and comments

---
+++ tests/OrderTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/* 
+ * Virgin Guitars E-Commerce Website 2016 - HomeAddressTest.php
+ * VGECW TEAM: Dominic Browne, Warren Norris, Dale Hogan, Ben Morrison
+ * 
+ * This file contains the unit tests to check the Order class
+ * To run these units tests type phpunit tests/Order.php from the website root
+ * For more details on running phpunit tests see the virgin guitars test plan
+ */
+
+use PHPUnit\Framework\TestCase;
+
+require_once 'classes/Order.php';
+
+class OrderTest extends TestCase 
+{
+    
+}
