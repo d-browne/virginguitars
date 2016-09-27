f09e0429065d3018aadd35efaee1d6f5c9a8b448
d-browne <d.browne@hackplex.com>
2016-09-27 22:31:16 +1000

    changes

---
+++ vieworder.php
@@ -0,0 +1,63 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// Include global header
+include 'includes/globalheader.php';
+
+require_once 'classes/Database.php'; // Include the database
+
+// Create data connection
+$database = new Database();
+$dataConnection = $database->getDataConnection();
+
+$errorString; // Contain error string
+
+// Check if id get received
+if (isset($_GET[id]))
+{
+    // Get order id from get request
+    $orderID = mysqli_real_escape_string($dataConnection, $_GET['id']);
+}
+
+// If member is not signed in redirect to sign in page
+if ($_SESSION["currentCustomer"]->getIsInitialized() !== true)
+{
+    header('Location: members.php');
+}
+
+// check if specified order exists
+$orderExists = false; // Flag
+
+// Query for the specified order
+$orderQuery = "SELECT * FROM PRODUCTS_BY_ORDER_VIEW WHERE SalesOrderFK ='".$orderID."' AND CustomerFK='".$_SESSION["currentCustomer"]->getCustomerID()."';";
+
+// Execute query
+$orderQueryResult = $dataConnection->query($orderQuery);
+
+// Check if query failed
+if ($orderQueryResult === false)
+{
+    $errorString = "Query Failed....";
+}
+
+// Check if result returned
+if ($orderQueryResult->num_rows < 0)
+{
+    // Set order flag on (order exists)
+    $errorString = "Order not found...";
+}
+
+if ($orderExists)
+{
+    echo 'success';
+}
+else
+{
+    
+}
+?>
