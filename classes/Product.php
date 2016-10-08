7995965c8abd44f374a5220a6a5764f5f87f5efd
d-browne <d.browne@hackplex.com>
2016-10-08 20:15:27 +1100

    added query for product

--- classes/Product.php
+++ classes/Product.php
@@ -8,6 +8,9 @@
  * 
  */
 
+// Require database
+require_once 'classes/Database.php';
+
 class Product
 {
     private $ProductID;
@@ -27,8 +30,31 @@ class Product
     private $ModifiedDate;
     
     // Class constructor
-    public function __construct($ProductID) 
-    {
+    public function __construct($ProductIDInput) 
+    {   
+        // Create data connection
+        $database = new Database();
+        $dataConnection = $database->getDataConnection();
         
+        // Sanitize input
+        $ProductID = mysqli_real_escape_string($dataConnection, $ProductIDInput);
+        
+        // Query to select specified product
+        $query = "SELECT * FROM PRODUCT_DETAILS_VIEW WHERE ProductID = '".$ProductID."';";
+        
+        //Execute query
+        $result = $dataConnection->query($query);
+        
+        // Throw if query failed
+        if ($result === false)
+        {
+            throw new Exception("Unable to query for product");
+        }
+        
+        // Throw if result not produced 
+        if ($result->num_rows < 1)
+        {
+            throw new Exception("ProductID not found");
+        }
     }
 }
