31f139cab579b160a29b5d8e5afa90b469e07cc7
d-browne <d.browne@hackplex.com>
2016-10-09 12:48:20 +1100

    Corrected case in view

--- dbscripts/PRODUCT_DETAILS_VIEW.sql
+++ dbscripts/PRODUCT_DETAILS_VIEW.sql
@@ -16,13 +16,13 @@ a.UserName As 'CreatedBy',
 a2.UserName As 'ModifiedBy',
 p.CreationDate,
 p.ModifiedDate
-FROM Product As p
-INNER JOIN Brand As b ON p.BrandFK = b.BrandID
-INNER JOIN Classification As c ON p.ClassificationFK = c.ClassificationID
-INNER JOIN Product_Status As ps ON p.StatusFK = ps.ProductStatusID
-INNER JOIN Appearence As ap ON p.AppearenceFK = ap.AppearenceID
-INNER JOIN Case_Type As ct ON p.CaseTypeFK = ct.CaseTypeID
-INNER JOIN Model As m ON p.ModelFK = m.ModelID
-INNER JOIN Administrator As a ON p.CreatedByFK = a.AdministratorID
-INNER JOIN Administrator As a2 ON p.ModifiedByFK = a2.AdministratorID
+FROM PRODUCT As p
+INNER JOIN BRAND As b ON p.BrandFK = b.BrandID
+INNER JOIN CLASSIFICATION As c ON p.ClassificationFK = c.ClassificationID
+INNER JOIN PRODUCT_STATUS As ps ON p.StatusFK = ps.ProductStatusID
+INNER JOIN APPEARENCE As ap ON p.AppearenceFK = ap.AppearenceID
+INNER JOIN CASE_TYPE As ct ON p.CaseTypeFK = ct.CaseTypeID
+INNER JOIN MODEL As m ON p.ModelFK = m.ModelID
+INNER JOIN ADMINISTRATOR As a ON p.CreatedByFK = a.AdministratorID
+INNER JOIN ADMINISTRATOR As a2 ON p.ModifiedByFK = a2.AdministratorID
 ORDER BY p.ProductID;
