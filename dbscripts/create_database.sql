35fd8cc32c21be54092328f6e2d22738cf583e45
d-browne <d.browne@hackplex.com>
2016-10-12 23:18:22 +1100

    fixed case in cart_view

--- dbscripts/create_database.sql
+++ dbscripts/create_database.sql
@@ -504,7 +504,7 @@ ORDER BY p.ProductID;
 
 # This view returns products in their cart
 CREATE VIEW CART_VIEW AS
-SELECT CART.CustomerFK, PRODUCT.PrimaryPicturePath, MODEL.Description, PRODUCT.ProductID, CART.Quantity, PRODUCT.UnitPrice As Price, (CART.Quantity*product.UnitPrice) As 'Total'
+SELECT CART.CustomerFK, PRODUCT.PrimaryPicturePath, MODEL.Description, PRODUCT.ProductID, CART.Quantity, PRODUCT.UnitPrice As Price, (CART.Quantity*PRODUCT.UnitPrice) As 'Total'
 FROM PRODUCT
 JOIN CART on CART.ProductFK = PRODUCT.ProductID
 JOIN MODEL on MODEL.ModelID = PRODUCT.ModelFK;
