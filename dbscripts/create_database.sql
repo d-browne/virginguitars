3efc059f9ae85ab9bb4cfe7b90310f998de40295
d-browne <d.browne@hackplex.com>
2016-10-16 18:07:57 +1100

    updated CART_VIEW to show status

--- dbscripts/create_database.sql
+++ dbscripts/create_database.sql
@@ -504,7 +504,8 @@ ORDER BY p.ProductID;
 
 # This view returns products in their cart
 CREATE VIEW CART_VIEW AS
-SELECT CART.CustomerFK, PRODUCT.PrimaryPicturePath, MODEL.Description, PRODUCT.ProductID, CART.Quantity, PRODUCT.UnitPrice As Price, (CART.Quantity*PRODUCT.UnitPrice) As 'Total', PRODUCT.isDeleted
+SELECT CART.CustomerFK, PRODUCT.PrimaryPicturePath, MODEL.Description, PRODUCT.ProductID, CART.Quantity, PRODUCT.UnitPrice As Price, (CART.Quantity*PRODUCT.UnitPrice) As 'Total', PRODUCT.isDeleted, PRODUCT_STATUS.Description As 'Status'
 FROM PRODUCT
 JOIN CART on CART.ProductFK = PRODUCT.ProductID
-JOIN MODEL on MODEL.ModelID = PRODUCT.ModelFK;
+JOIN MODEL on MODEL.ModelID = PRODUCT.ModelFK
+JOIN PRODUCT_STATUS on PRODUCT.StatusFK = PRODUCT_STATUS.ProductStatusID;
