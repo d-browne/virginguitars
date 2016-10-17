eecd70ca73433021cd5725b49c9dd39db6033ef3
d-browne <d.browne@hackplex.com>
2016-10-17 22:46:36 +1100

    added theme to process page

--- process.php
+++ process.php
@@ -1,6 +1,37 @@
 <?php
 include_once("includes/globalheader.php");
 include_once("classes/Database.php");
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Order</title>
+<link href="styles/main.css" rel="stylesheet" type="text/css">
+<link href="styles/js-image-slider.css" rel="stylesheet" type="text/css" />
+<script src="scripts/js-image-slider.js" type="text/javascript"></script>
+</head>
+
+<body>
+	<div id="mainBox">
+    	<?php
+            // Display Page header
+            include 'includes/pageheader.php.inc';
+        ?>
+        
+        <?php
+            // Display menu bar
+            include 'includes/menubar.php.inc';
+        ?>
+        
+        <?php
+            // Display menu bar
+            include 'includes/categorybox.php.inc';
+        ?>
+    <div id="contentBox">
+<?php
+
 
 include_once("paypalconfig.php");
 include_once("paypalfunctions.php");
@@ -178,3 +209,13 @@ include_once("paypal.class.php");
 		
 
 	}
+?>
+
+    </div>
+            <?php
+            // Display the page footer
+            include("includes/pagefooter.php.inc");
+            ?>
+        </div>
+</body>
+</html>
