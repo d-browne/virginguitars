2f96639f701636881f3b7c5b8cd40d969ae24192
d-browne <d.browne@hackplex.com>
2016-10-17 17:46:12 +1100

    updated paypal settings

--- paypalconfig.php
+++ paypalconfig.php
@@ -24,7 +24,7 @@
 	
 	define('PPL_LOGO_IMG', 'http://www.sanwebe.com/wp-content/themes/sanwebe/img/logo.png');
 	
-	define('PPL_RETURN_URL', 'http://localhost/paypalexample/process.php');
-	define('PPL_CANCEL_URL', 'http://localhost/paypalexample/cancel_url.php');
+	define('PPL_RETURN_URL', 'http://localhost/virginguitars/process.php');
+	define('PPL_CANCEL_URL', 'http://localhost/virginguitars/checkout.php');
 
 	define('PPL_CURRENCY_CODE', 'AUD');
